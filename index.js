f5268733708e27489f02cc0673d56b376678aa51
jahseed89 <samson.ocran@gmail.com>
2022-04-30 06:19:14 +0100

    qustions form updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', e => {
     e.preventDefault()
 
     let score = 0
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value]
+    const userAnswers = [form.option1.value, form.option2.value, form.option3.value, form.option4.value, form.option5.value, form.option6.value, form.option7.value, form.option8.value, form.option9.value, form.option10.value]
 
     // Checking the Answers
     userAnswers.forEach((answer, index) => {
